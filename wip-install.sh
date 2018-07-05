e8141f2ddb0ebca570e20cbd9d0fe3ebef8cc13a
ALRW <andrew.werner@fundingcircle.com>
2018-07-05 11:14:28 +0100

    [AW] added wip install script to track installed tooling

---
+++ wip-install.sh
@@ -0,0 +1,10 @@
+# A Work in progress script to track installed tooling to speed setup in future
+brew install ctags
+brew install htop
+brew install joker
+brew install leiningen
+brew install maven
+brew install pet
+brew install the_silver_searcher
+brew install tree
+brew install vim --override-system-vim
