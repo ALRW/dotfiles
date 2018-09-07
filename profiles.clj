413e80a4ed8696966ed945f2a5debaf4ff8975aa
ALRW <andrew.werner@fundingcircle.com>
2018-09-07 10:57:46 +0100

    [AW] Added profiles.clj to dotfiles

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[cider/cider-nrepl "0.17.0"]
+                  [venantius/ultra "0.5.2"]
+                  [venantius/yagni "0.1.4"]
+                  [lein-auto "0.1.3"]
+                  [lein-ancient "0.6.15"]]
+        :dependencies [[vvvvalvalval/scope-capture "0.2.0"]
+                       [cljfmt "0.5.1"]]}}
