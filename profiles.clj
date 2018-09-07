a2f062dc658647a46e12b542a5254b4e1aab165c
ALRW <andrew.werner@fundingcircle.com>
2018-09-07 17:35:25 +0100

    [AW] Added new lein dependencies

--- profiles.clj
+++ profiles.clj
@@ -2,6 +2,8 @@
                   [venantius/ultra "0.5.2"]
                   [venantius/yagni "0.1.4"]
                   [lein-auto "0.1.3"]
-                  [lein-ancient "0.6.15"]]
+                  [lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.6"]
+                  [lein-bikeshed "0.5.1"]]
         :dependencies [[vvvvalvalval/scope-capture "0.2.0"]
                        [cljfmt "0.5.1"]]}}
