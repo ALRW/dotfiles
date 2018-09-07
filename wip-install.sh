d882a28957d2da45a7dff7e60b098817e6a6ff95
ALRW <andrew.werner@fundingcircle.com>
2018-09-07 17:35:57 +0100

    [AW] Updated install script

--- wip-install.sh
+++ wip-install.sh
@@ -1,13 +1,41 @@
+#!/bin/bash
+
 # A Work in progress script to track installed tooling to speed setup in future
-brew install ctags
-brew install htop
-brew install joker
-brew install leiningen
-brew install maven
-brew install knqyf263/pet/pet
-brew install the_silver_searcher
-brew install tree
-brew install vim --override-system-vim
-brew install jq
-brew install fzf
-brew install wifi-password
+# Function to install brew packages
+brewInstall () {
+        echo "Installing: $1 with brew"
+        brew install $1
+}
+# Function to check if a command exists
+checkExists () {
+        command $1 >/dev/null 2>&1 ||
+                { echo >&2 "ERROR: $1 required but not installed"; exit 1;}
+}
+#Install rvm
+curl -sSL https://get.rvm.io | bash
+
+
+
+
+
+
+
+declare -a packages
+packages=(
+  "ctags"
+  "htop"
+  "joker"
+  "leiningen"
+  "maven"
+  "knqyf263/pet/pet"
+  "the_silver_searcher"
+  "tree"
+  "vim --override-system-vim"
+  "jq"
+  "fzf"
+  "wifi-password"
+)
+
+for ((i = 0; i < ${#packages[@]}; i++)); do
+        brewInstall ${packages[$i]}
+done
