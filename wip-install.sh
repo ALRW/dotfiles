aa8bc4363488634b39c32ef4e2be3d4251fe6c6b
ALRW <awerner1@googlemail.com>
2018-09-08 17:51:31 +0100

    [AW] some further quick updated to the wip-install script

--- wip-install.sh
+++ wip-install.sh
@@ -2,23 +2,17 @@
 
 # A Work in progress script to track installed tooling to speed setup in future
 # Function to install brew packages
+
+# The following are helper functions and data structures used by the script
 brewInstall () {
         echo "Installing: $1 with brew"
         brew install $1
 }
-# Function to check if a command exists
+# Function to check if a command exists and exit otherwise
 checkExists () {
         command $1 >/dev/null 2>&1 ||
                 { echo >&2 "ERROR: $1 required but not installed"; exit 1;}
 }
-#Install rvm
-curl -sSL https://get.rvm.io | bash
-
-
-
-
-
-
 
 declare -a packages
 packages=(
@@ -34,7 +28,31 @@ packages=(
   "jq"
   "fzf"
   "wifi-password"
+  "node"
+  "sbt"
+  "postgresql"
 )
+
+# The actuall installation script
+
+# Install the apple developer command line tools
+xcode-select install
+
+# Install rvm
+if checkExists curl; then
+        curl -sSL https://get.rvm.io | bash;
+fi
+
+# If we have rvm then upgrade to the latest version of ruby
+if checkExists rvm; then
+        rvm install ruby --latest;
+        rvm use --default --latest;
+fi
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
 
 for ((i = 0; i < ${#packages[@]}; i++)); do
         brewInstall ${packages[$i]}
