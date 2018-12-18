9f8bc55144639248f79b6b44cb689077d1c35830
ALRW <andrew.werner@fundingcircle.com>
2018-12-18 14:18:07 +0000

    [AW] fixed repl for clojure 1.10 and updated plugins and middleware

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.17.0"]
-                  [venantius/ultra "0.5.2"]
+{:user {:plugins [[cider/cider-nrepl "0.18.0"]
                   [venantius/yagni "0.1.4"]
+                  [mvxcvi/whidbey "2.0.0"]
                   [lein-auto "0.1.3"]
                   [lein-ancient "0.6.15"]
                   [lein-try "0.4.3"]
@@ -9,4 +9,5 @@
         :dependencies [[vvvvalvalval/scope-capture "0.2.0"]
                        [com.bhauman/rebel-readline "0.1.4"]
                        [cljfmt "0.5.1"]]
+        :middleware [whidbey.plugin/repl-pprint cider-nrepl.plugin/middleware]
         :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}}
